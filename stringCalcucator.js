0f2ff5b3dba0b2a99c6998903e1e6d90a4736a7c
Premal Katigar <premalk@Premals-MacBook-Pro.local>
2025-03-11 15:20:31 +0530

    Added Delimiters to get the sum of required string

--- stringCalcucator.js
+++ stringCalcucator.js
@@ -1,6 +1,21 @@
 function stringCalcucator(str) {
-    const strArr = str.split(",");
-    const intArr = strArr.map((value) => parseInt(value, 10));
+    let strArr;
+    if (str.includes(",")) {
+        strArr = str.split(",");
+    } else if (str.includes(";")) {
+        strArr = str.split(";");
+        for (let i=1; i<strArr.length; i++) {
+            if (strArr[i].includes("\n")) {
+                strArr[i].replace("\n", "");
+            }
+        }
+    }
+    const intArr = strArr.map((value) => {
+        if (Number(parseInt(value, 10))) {
+            return parseInt(value, 10);
+        }
+    }).filter((value) => value !== undefined);
+    console.log("intArr", intArr);
     const negatives = intArr.filter((value) => value < 0);
     if (negatives?.length) {
         return `negative numbers not allowed ${negatives?.join(",")}`
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -7,3 +7,7 @@ test("Addition of string '1,2,3' should be 6", () => {
 test("Addition of string '-1,2,3,-6' should print the message like negative numbers not allowed -1, -6", () => {
     expect(stringCalculator("-1,2,3,-6")).toBe("negative numbers not allowed -1,-6");
 });
+
+test("Addition of string '//;\n1;2' should be 3", () => {
+    expect(stringCalculator("//;\n1;2")).toBe(3);
+})
