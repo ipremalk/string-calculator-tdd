f6963ca7536fdfc32c97439b3add1650b1b2ee5f
Premal Katigar <premalk@Premals-MacBook-Pro.local>
2025-03-11 14:54:16 +0530

    Added

--- stringCalcucator.js
+++ stringCalcucator.js
@@ -1,8 +1,13 @@
 function stringCalcucator(str) {
     const strArr = str.split(",");
+    const intArr = strArr.map((value) => parseInt(value, 10));
+    const negatives = intArr.filter((value) => value < 0);
+    if (negatives?.length) {
+        return `negative numbers not allowed ${negatives?.join(",")}`
+    }
     let total = 0;
-    for (let i=0; i<strArr.length; i++) {
-        total += parseInt(strArr[i], 10);
+    for (let i=0; i<intArr.length; i++) {
+        total += intArr[i];
     }
     return total;
 };
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -3,3 +3,7 @@ const stringCalculator = require("./stringCalcucator");
 test("Addition of string '1,2,3' should be 6", () => {
     expect(stringCalculator("1,2,3")).toBe(6);
 });
+
+test("Addition of string '-1,2,3,-6' should print the message like negative numbers not allowed -1, -6", () => {
+    expect(stringCalculator("-1,2,3,-6")).toBe("negative numbers not allowed -1,-6");
+});
