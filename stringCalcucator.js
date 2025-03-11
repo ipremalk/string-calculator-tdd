b9782a84892c04cbac89d6dd3930023e09832a7b
Premal Katigar <premalk@Premals-MacBook-Pro.local>
2025-03-11 11:48:11 +0530

    String calculation added

--- stringCalcucator.js
+++ stringCalcucator.js
@@ -1,5 +1,10 @@
-function stringCalcucator(a, b) {
-    return a + b;
+function stringCalcucator(str) {
+    const strArr = str.split(",");
+    let total = 0;
+    for (let i=0; i<strArr.length; i++) {
+        total += parseInt(strArr[i], 10);
+    }
+    return total;
 };
 
 module.exports = stringCalcucator;
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -1,5 +1,5 @@
 const stringCalculator = require("./stringCalcucator");
 
-test("Addition of 1 and 2 should be 3", () => {
-    expect(stringCalculator(1, 2)).toBe(3);
+test("Addition of string '1,2,3' should be 6", () => {
+    expect(stringCalculator("1,2,3")).toBe(6);
 });
